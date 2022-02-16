39b85a5caf8452097fbe1597cef991d5ce604704
REZAN <asifrezan@gmail.com>
2022-02-17 03:29:04 +0600

    change static root

--- storage/settings.py
+++ storage/settings.py
@@ -122,12 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT='https://res.cloudinary.com/dnwqgkbv7/image/upload/v1/media/images'
+# STATIC_ROOT='https://res.cloudinary.com/dnwqgkbv7/image/upload/v1/media/images'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static")
-   
-)
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 STATIC_URL = 'static/'
