ffb287b5cd116448e80af6bf59ee28cf6be04d44
Md. Shahriar Asif Rezan <asifrezan@gmail.com>
2022-02-19 21:58:22 +0600

    Add static root url

--- storage/settings.py
+++ storage/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 #STATIC_ROOT='https://res.cloudinary.com/dnwqgkbv7/static'
 
